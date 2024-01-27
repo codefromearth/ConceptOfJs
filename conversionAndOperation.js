60566e1494ea34c5c94c6cf424f7935c9d4188ff
Raushan <raushan.kumar@bayatree.com>
2024-01-27 18:41:27 +0530

    conversion of data Type

---
+++ conversionAndOperation.js
@@ -0,0 +1,16 @@
+let score ='33abc'
+console.log(typeof score);
+
+let ValueInNumber=Number(undefined)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+console.log(typeof NaN);
+
+
+//"33"=>33
+//"33abc"=>Nan  but type number
+let isLoggedIn=[]
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log("booleanIsLoggedIn",booleanIsLoggedIn)
+
