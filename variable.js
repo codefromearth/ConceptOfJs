17e3e298aa7aaf7d6559954a7c94048a84f7d71f
Raushan <raushan.kumar@bayatree.com>
2024-01-27 18:26:09 +0530

    data type in javascript

---
+++ variable.js
@@ -0,0 +1,22 @@
+const accountId=14423;
+let accountEmail="raushan@gmail.com";
+var accountPassword="9854";
+
+accountCity='chatra'
+// accountId=1234
+console.log(accountId);
+
+//type of datatype
+//number=> 2 to power 53
+//bigint=> large int
+//boolean=>true /false
+//null=> standalone value/intenstionally empty
+//undefined=>not assign any value to variable
+//symbol=>unique
+
+
+//object
+
+
+console.log(typeof undefined);//undefined
+console.log(typeof null)//object
