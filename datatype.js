68df308e0fd733c0a7e316fd54e2fc644cda35c6
Raushan <raushan.kumar@bayatree.com>
2024-01-27 20:33:12 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,14 @@
+//Primitive
+// 7 type: Strin,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+//Refrence Type  (Non premitive)
+
+//Array,  Object, Functions
+
+const id=Symbol('123')
+const id2=Symbol('123')
+
+//console.log(id==id2,"-------------------");
+
+const arr=[1,2,3,4,5]
+console.log(typeof arr,typeof id);//object,symbol
